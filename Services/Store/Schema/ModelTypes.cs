fb23b0c03ebcdc5d67bef81e3f42861c144d95bd
Peter Kalivas <peterkalivas@gmail.com>
2024-02-21 07:44:05 -0600

    asdf

---
+++ Services/Store/Schema/ModelTypes.cs
@@ -0,0 +1,8 @@
+namespace Radiate.Client.Services.Store.Schema;
+
+public static class ModelTypes
+{
+    public const string Graph = "Graph";
+    public const string Tree = "Tree";
+    public const string Image = "Image";
+}
